9ff347058019f46b1942723e426ded6b5120be60
akio0911 <akio0911@gmail.com>
2021-08-14 19:40:02 +0900

    feedback

--- Exercise_01/AnotherPatternViewController.swift
+++ Exercise_01/AnotherPatternViewController.swift
@@ -9,45 +9,49 @@ import UIKit
 //課題別パターン
 class AnotherPatternViewController: UIViewController,UITextFieldDelegate {
     
-    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet private var resultLabel: UILabel!
     
     //TextFieldの数
-    private var requiredNumber = 5 //今回の課題の数分
+    private let requiredNumber = 5 //今回の課題の数分
     private var textFields = [UITextField]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textFieldGeneration()
-        // Do any additional setup after loading the view.
+        generateTextFields()
     }
 
     //MARK:METHODs
     
     //計算実行
-    @IBAction func calculationButton(_ sender: Any){
-        calculation()
+    @IBAction func calculationButton(_ sender: Any) {
+        calculate()
     }
+
     //textField生成
-    private func textFieldGeneration(){
+    private func generateTextFields() {
         for i in 0..<requiredNumber {
             let textField = UITextField().textFieldLayout(i: i)
             textField.delegate = self
             textFields.append(textField)
-            self.view.addSubview(textField)
+            view.addSubview(textField)
         }
     }
+
     ///計算処理
-    private func calculation(){
-        var result = Int()
-        for i in 0..<textFields.count{
-            result += Int(textFields[i].text!) ?? 0
+    private func calculate() {
+        var result = 0
+
+        textFields.forEach {
+            result += Int($0.text!) ?? 0
         }
+
         resultLabel.text = String(result)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.endEditing(true)
     }
@@ -56,13 +60,13 @@ class AnotherPatternViewController: UIViewController,UITextFieldDelegate {
 
 
 extension UITextField{
-    func textFieldLayout(i:Int) -> UITextField{
-        self.borderStyle = .roundedRect
-        self.layer.borderColor = UIColor.lightGray.cgColor
-        self.textColor = .black
-        self.backgroundColor = .lightGray
-        self.keyboardType = .numberPad
-        self.frame = CGRect(x: 20, y: 100 + i * 40, width: 250, height: 30)
+    func textFieldLayout(i: Int) -> UITextField {
+        borderStyle = .roundedRect
+        layer.borderColor = UIColor.lightGray.cgColor
+        textColor = .black
+        backgroundColor = .lightGray
+        keyboardType = .numberPad
+        frame = CGRect(x: 20, y: 100 + i * 40, width: 250, height: 30)
         return self
     }
 }
